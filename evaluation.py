f8fb5ae147787c27f957b9ef9e41739bf5c097f0
falconlee236 <falconlee236@cau.ac.kr>
2025-02-16 21:15:05 +0900

    add evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,42 +1,16 @@
-from transformers import pipeline
-import dotenv
 import os
-import huggingface_hub
+import re
+import dotenv
+from transformers import pipeline, Pipeline
+from huggingface_hub import login, InferenceClient
 from datasets import load_dataset
 
+# https://github.com/HeegyuKim/open-korean-instructions?tab=readme-ov-file 
+# https://huggingface.co/learn/cookbook/llm_judge
 dotenv.load_dotenv()
 API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 REPO_NAME = os.getenv("HUGGINGFACE_REPO")
-
-huggingface_hub.login(
-    token=os.getenv("HUGGINGFACE_API_KEY")
-)
-
-pipe = pipeline(
-    task="text-generation",
-    model=REPO_NAME,
-    tokenizer=REPO_NAME,
-    max_length=256
-)
- 
-def generate_and_stop(instructions: list) -> list:
-    prompt = """
-    <|begin_of_text|><|start_header_id|>system<|end_header_id|>
-    
-    You are a helpful assistant<|eot_id|>\n<|start_header_id|>user<|end_header_id|>
-    
-    {question}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>
-    """
-    results = []
-    for instruction in instructions:
-        results.append(pipe(f"{prompt.format(instruction)}")[0]['generated_text'])
-    return results
- 
-
-
-ds = load_dataset("HAERAE-HUB/KUDGE")
-
-
+JUDGE_MODEL_ID = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 JUDGE_PROMPT = """
 사용자 질문(user_question)과 시스템 답변(system_answer) 쌍이 주어질 것입니다. 귀하의 임무는 시스템 답변이 사용자 질문에서 표현된 사용자의 관심사를 얼마나 잘 답변했는지 '종합 평가'를 제공하는 것입니다. 
 
@@ -58,6 +32,80 @@ JUDGE_PROMPT = """
 종합 평가:
 """
 
-results = generate_and_stop([x for x in ds["instruction"]])
-for x in results:
-    print(x)
+def extract_judge_score(answer: str, split_str: str = "Total rating:") -> int:
+    try:
+        if split_str in answer:
+            rating = answer.split(split_str)[1]
+        else:
+            rating = answer
+        digit_groups = [el.strip() for el in re.findall(r"\d+(?:\.\d+)?", rating)]
+        return float(digit_groups[0])
+    except Exception as e:
+        print(e)
+        return 0
+
+
+def generate_and_stop(pipe:Pipeline, instructions: list, llm: InferenceClient) -> list:
+    prompt = """
+    <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+    
+    You are a helpful assistant<|eot_id|>\n<|start_header_id|>user<|end_header_id|>
+    
+    {question}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>
+    """
+    results = []
+    for instruction_dict in instructions:
+        eval_model_input = instruction_dict["instruction"]
+
+        eval_model_output = pipe(f"{prompt.format(
+            question=eval_model_input,
+        )}")[0]['generated_text']
+
+        test_model_prompt = JUDGE_PROMPT.format(
+            question=eval_model_input,
+            answer=eval_model_output,
+        )
+
+        llm_answer = llm.text_generation(
+            prompt=test_model_prompt,
+            max_new_tokens=20,
+        )
+        results.append({
+            **instruction_dict,
+            "judge_score": extract_judge_score(answer=llm_answer),
+        })
+    return results
+ 
+
+if __name__ == "__main__":
+    login(token=os.getenv("HUGGINGFACE_API_KEY"))
+
+    llm_client = InferenceClient(
+        model=JUDGE_MODEL_ID,
+        timeout=120,
+    )
+    
+
+    pipeline = pipeline(
+        task="text-generation",
+        model=REPO_NAME,
+        tokenizer=REPO_NAME,
+        max_length=256
+    )
+
+    hub_datasets = load_dataset("HAERAE-HUB/KUDGE", "Human Annotations")
+    human_datasets = [
+    dict(uuid=item["uuid"],
+        instruction=item["instruction"],
+        response=item["response"],
+    ) for item in hub_datasets["test"]]
+
+    ans = generate_and_stop(
+        pipe=pipeline,
+        instructions=human_datasets,
+        llm=llm_client,
+    )
+
+    print(len(ans))
+    for x in ans:
+        print(x)
